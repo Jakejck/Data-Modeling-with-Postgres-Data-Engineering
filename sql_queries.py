b9726b423d33ac0e487fd09dac11488e49bd628e
Jake <84796183@qq.com>
2020-03-16 22:29:24 +0800

    update etl.ipynb (artist table & time table)

--- sql_queries.py
+++ sql_queries.py
@@ -17,8 +17,9 @@ gender varchar,level varchar);")
 
 song_table_create = ("CREATE TABLE IF NOT EXISTS songs (song_id varchar,title varchar,artist_id varchar,year int,duration numeric);")
 
-artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (artist_id int,name varchar,location varchar,\
-                      latitude decimal,longitude decimal);")
+
+artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (artist_id varchar,name varchar,location varchar,\
+                      latitude numeric,longitude numeric);")
 
 time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time time,hour int,day int,week int,\
                      month int, year int, weekday int);")
@@ -33,8 +34,7 @@ user_table_insert = ("""
 
 song_table_insert = ("INSERT INTO songs (song_id,title,artist_id,year,duration) VALUES (%s,%s, %s, %s, %s)")
 
-artist_table_insert = ("""
-""")
+artist_table_insert = ("INSERT INTO artists (artist_id,name,location,latitude,longitude) VALUES (%s,%s, %s, %s, %s)")
 
 
 time_table_insert = ("""
